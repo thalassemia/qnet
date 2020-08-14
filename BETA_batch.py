0f67a5e2b34bd3b29f75e9d9f2260883d1b85b7b
thalassemia <67928790+thalassemia@users.noreply.github.com>
2020-08-13 20:21:17 -0700

    Update to BETA minus

--- BETA_batch.py
+++ BETA_batch.py
@@ -8,7 +8,7 @@ peakdir = '/home/sean/peaks/'
 
 def beta(name, root):
 
-    args = shlex.split('BETA minus -p ' + name + ' -k BSF --gname2 -g hg38 --da 500 -n ' + os.path.dirname(os.path.join(root, name)) + '_' + re.sub('[^0-9]', '', name) + ' -o /home/sean/targets/')
+    args = shlex.split('BETA minus -p ' + name + ' -g hg38 -n ' + os.path.dirname(os.path.join(root, name)) + '_' + re.sub('[^0-9]', '', name) + ' -o /home/sean/targets/')
     print(name + subprocess.call(args, universal_newlines=True, cwd=root))
 
 
