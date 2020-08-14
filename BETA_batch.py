be272d16ef5990476576fc0fdb57cb4f8d254c60
thalassemia <67928790+thalassemia@users.noreply.github.com>
2020-08-13 20:39:25 -0700

    Allow custom output directory

--- BETA_batch.py
+++ BETA_batch.py
@@ -5,10 +5,11 @@ import re
 import concurrent.futures
 
 peakdir = '/home/sean/peaks/'
+outdir = '/home/sean/targets/'
 
 def beta(name, root):
 
-    args = shlex.split('BETA minus -p ' + name + ' -g hg38 -n ' + os.path.dirname(os.path.join(root, name)) + '_' + re.sub('[^0-9]', '', name) + ' -o /home/sean/targets/')
+    args = shlex.split('BETA minus -p ' + name + ' -g hg38 -n ' + os.path.dirname(os.path.join(root, name)) + '_' + re.sub('[^0-9]', '', name) + ' -o ' + outdir)
     print(name + subprocess.call(args, universal_newlines=True, cwd=root))
 
 
