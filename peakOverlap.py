2667a8490d36916b94b6b29dafbf9943828f559a
thalassemia <67928790+thalassemia@users.noreply.github.com>
2020-08-20 22:33:47 -0700

    Find overlap only using one file per cofactor

--- peakOverlap.py
+++ peakOverlap.py
@@ -8,27 +8,25 @@ from pathlib import Path
 import concurrent.futures
 from tqdm import tqdm
 
-peakDir = "/home/sean/peaks/"
-outDir = "/home/sean/coassoc/"
+peakDir = "/home/sean/TFChIP/goodBeds/"
+outDir = "/home/sean/TFChIP/intersect/"
 
 def intersect(factor):
     cofactors = next(os.walk(peakDir))[1]
+    a = glob.glob(peakDir + factor + "/*.bed")[0]
     for cofactor in cofactors:
-        if cofactor == factor:
-            continue
-        for a in glob.glob(peakDir + factor + "/*.bed"):
-            for b in glob.glob(peakDir + cofactor + "/*.bed"):
-                command = shlex.split(f"/home/sean/bedtools intersect -a {a} -b {b} -c")
-                result = subprocess.run(command, stdout=subprocess.PIPE)
-                output = result.stdout.decode('utf-8')
-                df = pd.read_csv(io.StringIO(output), sep="\t")
-                fileIDs = (a.split('/')[-1]).split('_')[0] + "_" + cofactor + "_" + (b.split('/')[-1]).split('_')[0] + ".csv"
-                p = Path(f'{outDir}/{factor}/')
-                p.mkdir(exist_ok=True)
-                df.to_csv(f"{p}/{fileIDs}", sep=",", index=False)
+        b = glob.glob(peakDir + cofactor + "/*.bed")[0]
+        command = shlex.split(f"/home/sean/bedtools intersect -a {a} -b {b} -loj")
+        result = subprocess.run(command, stdout=subprocess.PIPE)
+        output = result.stdout
+        df = pd.read_csv(io.BytesIO(output), sep="\t", header = None)
+        fileIDs = ((a.split('/')[-1]).split('.')[0]).split('*')[0] + "_" + cofactor + "_" + ((b.split('/')[-1]).split('.')[0]).split('*')[0] + ".csv"
+        p = Path(f'{outDir}/{factor}/')
+        p.mkdir(exist_ok=True)
+        df.to_csv(f"{p}/{fileIDs}", index=False, header = False)
+    return
 
 filesystem = os.walk(peakDir)
 factors = next(filesystem)[1]
-
 with concurrent.futures.ProcessPoolExecutor() as executor:
-    tqdm(executor.map(intersect, factors), total=len(factors))
+    list(tqdm(executor.map(intersect, factors), total=len(factors)))
