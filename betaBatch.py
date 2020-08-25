a659d91b49cdb8cf1843916a876123456ed471d9
thalassemia <67928790+thalassemia@users.noreply.github.com>
2020-08-25 00:24:50 -0700

    Buggy ProcessPoolExecutor in futures for Python2.7

--- betaBatch.py
+++ betaBatch.py
@@ -2,16 +2,17 @@ import subprocess
 import shlex
 import os
 import re
-import concurrent.futures
+from multiprocessing import Pool
 
 peakdir = '/home/sean/peaks/'
 outdir = '/home/sean/targets/'
 
 def beta(name, root):
-
     args = shlex.split('BETA minus -p ' + name + ' -g hg38 -n ' + os.path.dirname(os.path.join(root, name)) + '_' + re.sub('[^0-9]', '', name) + ' -o ' + outdir)
     print(name + subprocess.call(args, universal_newlines=True, cwd=root))
 
+def unpack(args):
+    beta(*args)
 
 names = []
 roots = []
@@ -19,5 +20,5 @@ for root, dirs, files in os.walk(peakdir):
     for name in files:
         names.append(name)
         roots.append(root)
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    executor.map(beta, names, roots)
+p = Pool(8)
+p.map(beta, zip(names, roots))
