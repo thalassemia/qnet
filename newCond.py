8081d427b6117b750ab1a96f707209a057b84627
thalassemia <67928790+thalassemia@users.noreply.github.com>
2021-02-08 01:58:46 -0800

    Create correlation plots

--- newCond.py
+++ newCond.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import pearsonr, hypergeom
 
-os.makedirs(os.path.expandvars("$SCRATCH/compareCond/"), exist_ok = True)
+os.makedirs(os.path.expandvars("$SCRATCH/compareCond/plots/"), exist_ok = True)
 tfPath = os.path.expandvars("$SCRATCH/data/Database.csv")
 tf = pd.read_csv(tfPath)
 tf.rename(columns = {"HGNC symbol": "gene_name"}, inplace = True)
@@ -61,10 +61,8 @@ deGenes = [ss, ci, sir, cir]
 outNames = ['ss', 'ci', 'sir', 'cir']
 deTFs = [ss_tfs, ci_tfs, sir_tfs, cir_tfs]
 
-same_gene = np.zeros((4,4))
-diff_gene = np.zeros((4,4))
-same_tf = np.zeros((4,4))
-diff_tf = np.zeros((4,4))
+gene = np.zeros((4,4))
+tf = np.zeros((4,4))
 
 for i in range(4):
     for j in range(i,4):
@@ -75,47 +73,33 @@ for i in range(4):
         # theoretically there should be no output here because i == j means we are comparing the same files
         if i == j:
             print(diff)
+        # create plot showing correlation between the log 2 fold changes of DE genes
+        plt.scatter(merged.loc[merged["_merge"] == "both"]['log2FoldChange_x'], merged.loc[merged["_merge"] == "both"]['log2FoldChange_y'], c = "steelblue", label = 'DE Genes', alpha = 0.5)
+        coef = np.polyfit(merged.loc[merged["_merge"] == "both"]['log2FoldChange_x'], merged.loc[merged["_merge"] == "both"]['log2FoldChange_y'], 1)
+        eq2 = np.poly1d(coef)
+        r2, p2 = pearsonr(merged.loc[merged["_merge"] == "both"]['log2FoldChange_x'], merged.loc[merged["_merge"] == "both"]['log2FoldChange_y'])
+        plt.plot(merged.loc[merged["_merge"] == "both"]['log2FoldChange_x'], eq2(merged.loc[merged["_merge"] == "both"]['log2FoldChange_x']), c = 'r', label = f'r = {round(r2, 3)}')
         # count the number of tfs that appear in both compared lists and change in the same or different directions
-        same_gene[i,j] = np.sum(same['_merge'] == 'both')
-        diff_gene[i,j] = np.sum(diff['_merge'] == 'both')
+        gene[j,i] = np.sum(diff['_merge'] == 'both')
+        gene[i,j] = np.sum(same['_merge'] == 'both')
         merged = deTFs[i][["Ensembl ID", "gene_name", "log2FoldChange", "padj"]].merge(deTFs[j][["Ensembl ID", "gene_name", "log2FoldChange", "padj"]], on = "Ensembl ID", indicator = True, how = "outer")
         merged.to_csv(os.path.expandvars("$SCRATCH/compareCond/" + outNames[i] + "_" + outNames[j] + "_tfs.csv"))
         same = merged.loc[np.sign(merged.loc[:,'log2FoldChange_x']) == np.sign(merged.loc[:,'log2FoldChange_y'])]
         diff = merged.loc[np.sign(merged.loc[:,'log2FoldChange_x']) != np.sign(merged.loc[:,'log2FoldChange_y'])]
         # count the number of tfs that appear in both compared lists and change in the same or different directions
-        same_tf[i,j] = np.sum(same['_merge'] == 'both')
-        diff_tf[i,j] = np.sum(diff['_merge'] == 'both')
-np.savetxt(os.path.expandvars("$SCRATCH/compareCond/same_gene.csv"), same_gene)
-np.savetxt(os.path.expandvars("$SCRATCH/compareCond/diff_gene.csv"), diff_gene)
-np.savetxt(os.path.expandvars("$SCRATCH/compareCond/same_tf.csv"), same_tf)
-np.savetxt(os.path.expandvars("$SCRATCH/compareCond/diff_tf.csv"), diff_tf)
-
-# merged dataframe would normally go here
-ss_ci = pd.DataFrame()
-
-# naive attempt at hypergeometric test to assess significance of the overlap observed between
-b = ss_ci.shape[0]
-s = ss_tfs.shape[0]
-c = ci_tfs.shape[0]
-t = tf.shape[0]
-pval = hypergeom.sf(b-1, t, s, c)
-
-table = pd.DataFrame({"In SSvP": [b, s-b, s, "NA"], "Not in SSvP": [c-b, t-c-s+b, t-s, "NA"], "Total": [c, t-c, t, pval]}, index = ["In CIvP", "Not in CIvP", "Total", "Hypergeometric p-value"])
-table.to_csv(os.path.expandvars("$SCRATCH/compareCond/table.csv"))
-
-plt.scatter(merged.loc[merged["_merge"] == "both"]["l2FC_SSvP"], merged.loc[merged["_merge"] == "both"]["l2FC_CIvP"], c = "steelblue", label = 'DE Genes', alpha = 0.5)
-coef = np.polyfit(merged.loc[merged["_merge"] == "both"]["l2FC_SSvP"], merged.loc[merged["_merge"] == "both"]["l2FC_CIvP"], 1)
-eq2 = np.poly1d(coef)
-r2, p2 = pearsonr(merged.loc[merged["_merge"] == "both"]["l2FC_SSvP"], merged.loc[merged["_merge"] == "both"]["l2FC_CIvP"])
-plt.plot(merged.loc[merged["_merge"] == "both"]["l2FC_SSvP"], eq2(merged.loc[merged["_merge"] == "both"]["l2FC_SSvP"]), c = 'r', label = f'r = {round(r2, 3)}')
-
-# plot showing very strong correlation between the log 2 fold changes of genes in all conditions
-plt.scatter(ss_ci["l2FC_SSvP"], ss_ci["l2FC_CIvP"], c = 'darkorange', label = 'DE TFs', alpha = 0.5)
-plt.xlabel("SSvsP Log 2 Fold Change")
-plt.ylabel("CIvsP Log Fold Change")
-coef = np.polyfit(ss_ci["l2FC_SSvP"], ss_ci["l2FC_CIvP"], 1)
-eq1 = np.poly1d(coef)
-r1, p1 = pearsonr(ss_ci["l2FC_SSvP"], ss_ci["l2FC_CIvP"])
-plt.plot(ss_ci["l2FC_SSvP"], eq1(ss_ci["l2FC_SSvP"]), c = 'darkorange', label = f'r = {round(r1, 3)}')
-plt.legend()
-plt.savefig(os.path.expandvars("$SCRATCH/compareCond/corr.png"))
+        tf[j,i] = np.sum(diff['_merge'] == 'both')
+        tf[i,j] = np.sum(same['_merge'] == 'both')
+        # overlay plot showing correlation between the log 2 fold changes of DE TFs
+        merged = merged.loc[merged['_merge'] == 'both']
+        plt.scatter(merged["log2FoldChange_x"], merged["log2FoldChange_y"], c = 'darkorange', label = 'DE TFs', alpha = 0.5)
+        plt.xlabel(outNames[i].upper() + " Log 2 Fold Change")
+        plt.ylabel(outNames[j].upper() + " Log 2 Fold Change")
+        coef = np.polyfit(merged["log2FoldChange_x"], merged['log2FoldChange_y'], 1)
+        eq1 = np.poly1d(coef)
+        r1, p1 = pearsonr(merged["log2FoldChange_x"], merged['log2FoldChange_y'])
+        plt.plot(merged["log2FoldChange_x"], eq1(merged["log2FoldChange_x"]), c = 'darkorange', label = f'r = {round(r1, 3)}')
+        plt.legend()
+        plt.savefig(os.path.expandvars("$SCRATCH/compareCond/plots/" + outNames[i] + "_" + outNames[j] + ".png"))
+        plt.clf()
+np.savetxt(os.path.expandvars("$SCRATCH/compareCond/gene_counts.csv"), gene)
+np.savetxt(os.path.expandvars("$SCRATCH/compareCond/tf_counts.csv"), tf)
