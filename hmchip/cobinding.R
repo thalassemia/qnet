06b745448913d28c5577448502377ab118302aff
thalassemia <67928790+thalassemia@users.noreply.github.com>
2021-02-08 01:59:28 -0800

    Prevent C stack error

--- hmchip/cobinding.R
+++ hmchip/cobinding.R
@@ -14,13 +14,15 @@ ca <- t(ca[,-1])
 pdf(paste0(cobindDir,"atac.pdf"))
 pheatmap(
     mat               = as.matrix(ca),
-    cluster_cols      = sort_hclust(hclust.vector(t(ca), method="ward")),
-    cluster_rows      = sort_hclust(hclust.vector(ca, method="ward")),
+    cluster_cols      = hclust.vector(t(ca), method="ward"),
+    cluster_rows      = hclust.vector(ca, method="ward"),
+    #cluster_cols      = sort_hclust(hclust.vector(t(ca), method="ward")),
+    #cluster_rows      = sort_hclust(hclust.vector(ca, method="ward")),
     fontsize          = 12,
     treeheight_row    = 0, 
     treeheight_col    = 0,
     show_colnames     = F,
     fontsize_row      = 4,
-    main              = "ATAC-Seq Diff. Open Co-Binding Map",
+    main              = "DA Region Co-Binding Map",
     color             = colorRampPalette(brewer.pal(9,"Reds"))(400))
 dev.off()
