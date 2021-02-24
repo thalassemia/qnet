6de7e244c8fc5f39e04b539d427435e9241d6db1
thalassemia <67928790+thalassemia@users.noreply.github.com>
2021-02-23 16:09:53 -0800

    Increased randomization

--- tfchip/ml.py
+++ tfchip/ml.py
@@ -9,16 +9,17 @@ def main(args):
     import numpy as np
     import time
     import random
-    from itertools import compress
+    from itertools import compress, permutations
     import os
     import shap
     import lightgbm as lgb
     import shelve
     import re
+    import glob
 
-    def model(number, exp, control):
-        print(exp)
-        p = pd.read_csv("/u/scratch/s/seanchea/cobinding/cobind/" + exp, sep = "\t", index_col = 0)
+    def model(condition, sets):
+        print(sets)
+        p = pd.read_csv(sets[0], sep = "\t", index_col = 0)
         # independently shuffle each column, theoretically eliminating any TF binding dependencies
         n = p.apply(lambda x: x.sample(frac = 1).values) # .values needed or else will return Series that realigns via index
 
@@ -31,7 +32,7 @@ def main(args):
         X.columns = features
         X.shape[0] / 2
 
-        p = pd.read_csv("/u/scratch/s/seanchea/cobinding/cobind/" + control, sep = "\t", index_col = 0)
+        p = pd.read_csv(sets[1], sep = "\t", index_col = 0)
         n = p.apply(lambda x: x.sample(frac = 1).values)
 
         p["y"] = [1] * len(p)
@@ -68,10 +69,12 @@ def main(args):
         print(f'Done with interactions in {time.time() - start} sec')
 
         # export all relevant variables for future reference
-        saveDir=f'/u/scratch/s/seanchea/models/{exp}/{number}/python/test/'
+        train_num = sets[0].split("/")[-1].split("_")[-1].split(".")[0]
+        test_num = sets[1].split("/")[-1].split("_")[-1].split(".")[0]
+        saveDir=f'/u/scratch/s/seanchea/models/{condition}/{str(train_num)}_{str(test_num)}/'
         os.makedirs(saveDir, exist_ok = True)
-        my_shelf = shelve.open(saveDir + "/saveData" ,'n', protocol = 4)
-
+        
+        my_shelf = shelve.open(saveDir + "saveData")
         my_shelf["trees"] = locals()["trees"]
         my_shelf["X"] = locals()["X"]
         my_shelf["X_test"] = locals()["X_test"]
@@ -83,49 +86,13 @@ def main(args):
         my_shelf.close()
 
         print('Done saving all variables (with test set)')
+    
+    print(sys.argv[:])
+    maps = glob.glob(os.path.expandvars("$SCRATCH/cobinding/cobind/") + args[-1] + "_*.csv")
+    combos = permutations(maps, 2)
 
-        # repeat procedure for model validated on itself
-        start = time.time()
-        trees = lgb.train(param, train_data, 100, valid_sets = train_data, feature_name=features, verbose_eval=1)
-        print(f'Done training in {time.time() - start} sec')
-
-        #TO-DO: implement more sophisticated evaluation measure like AUROC
-        print(f'Train set: {np.mean((trees.predict(X) > 0.5) == y)}')
-        print(f'Test set: {np.mean((trees.predict(X_test) > 0.5) == y_test)}')
-
-        start = time.time()
-        explainer = shap.TreeExplainer(trees)
-        shap_values = explainer.shap_values(X)
-        print(f'Done with SHAP values in {time.time() - start} sec')
-
-        cores = mp.cpu_count()
-        chunks = np.array_split(X, cores)
-        start = time.time()
-        # having SHAP calculate interaction values on chunks to enable multiprocessing
-        with concurrent.futures.ProcessPoolExecutor(cores) as executor:
-            a = executor.map(explainer.shap_interaction_values, chunks)
-        allInteractions = np.concatenate(list(a), axis = 0)
-        print(f'Done with interactions in {time.time() - start} sec')
-
-        # export all relevant variables for future reference
-        saveDir=f'/u/scratch/s/seanchea/models/{exp}/{number}/python/self/'
-        os.makedirs(saveDir, exist_ok = True)
-        my_shelf = shelve.open(saveDir + "/saveData" ,'n', protocol = 4)
-
-        my_shelf["trees"] = locals()["trees"]
-        my_shelf["X"] = locals()["X"]
-        my_shelf["X_test"] = locals()["X_test"]
-        my_shelf["allInteractions"] = locals()["allInteractions"]
-        my_shelf["shap_values"] = locals()["shap_values"]
-        my_shelf["features"] = locals()["features"]
-        my_shelf["explainer"] = locals()["explainer"]
-
-        my_shelf.close()
-
-        print('Done saving all variables')
-
-    for i in range(100): 
-        model(i, args[-2], args[-1])
+    for i in combos: 
+        model(args[-1], i)
 
 # this convoluted setup is necessary to enable the "spawn" flag to be set properly
 if __name__ == '__main__':
--- tfchip/preprocess/prep.py
+++ tfchip/preprocess/prep.py
@@ -62,16 +62,19 @@ def intersect(a):
     os.makedirs(os.path.join(intDir, factor), exist_ok = True)
     merged.to_csv(os.path.join(intDir, factor, bedname), sep = "\t", index = False)
 
-with concurrent.futures.ProcessPoolExecutor(36) as executor:
+""" with concurrent.futures.ProcessPoolExecutor(36) as executor:
     beds = glob.glob(normDir + "*/*.bed")
-    list(tqdm(executor.map(intersect, beds), total = len(beds)))
+    list(tqdm(executor.map(intersect, beds), total = len(beds))) """
 
-# get rank of bed file (for Cistrome) or assign rank of 0 (preference to ENCODE files)
+# get rank of bed file (add 1000 to Cistrome ranks to always prefer ENCODE)
+encodeKey = pd.read_csv(os.path.expandvars("$SCRATCH/cobinding/beds/encodeKey.csv"))
+encodeKey.loc[:,"Path"] = [i.split("/")[-1] for i in encodeKey["Path"]]
 def num(x):
     try:
-        return int(x.split("/")[-1].split(".bed")[0])
+        return int(x.split("/")[-1].split(".bed")[0]) + 1000
     except:
-        return 0
+        if x.split("/")[-1] in encodeKey.loc[:,"Path"].values:
+            return encodeKey.index[encodeKey["Path"] == x.split("/")[-1]].tolist()[0]
 
 factors = os.listdir(intDir)
 sortedBeds = []
@@ -80,117 +83,64 @@ for factor in factors:
     beds.sort(key = num)
     sortedBeds.append(beds)
 
-# compile cobinding maps for different conditions
-cobindUU = pd.DataFrame() # upregulated TFs at upregulated DE genes
-cobindUD = pd.DataFrame() # upregulated TFs at downregulated DE genes
-cobindDD = pd.DataFrame() # downregulated TFs at downregulated DE genes
-cobindDU = pd.DataFrame() # downregulated TFs at upregulated DE genes
-cobindUp = pd.DataFrame() # upregulated TFs at all DE genes
-cobindDown = pd.DataFrame() # downregulated TFs at all DE genes
-cobind_Up = pd.DataFrame() # all DE TFs at upregulated DE genes
-cobind_Down = pd.DataFrame() # all DE TFs at downregulated DE genes
-cobindAll = pd.DataFrame() # all DE TFs at all DE genes
-deGenes = []
+# create 11 cobinding maps using different sets of data each time if possible so there can be 11*10 = 110 combinations of training and test sets for 110 models
+for count in range(11):
+    # compile cobinding maps for different conditions
+    cobindUU = pd.DataFrame() # upregulated TFs at upregulated DE genes
+    cobindUD = pd.DataFrame() # upregulated TFs at downregulated DE genes
+    cobindDD = pd.DataFrame() # downregulated TFs at downregulated DE genes
+    cobindDU = pd.DataFrame() # downregulated TFs at upregulated DE genes
+    cobindUp = pd.DataFrame() # upregulated TFs at all DE genes
+    cobindDown = pd.DataFrame() # downregulated TFs at all DE genes
+    cobind_Up = pd.DataFrame() # all DE TFs at upregulated DE genes
+    cobind_Down = pd.DataFrame() # all DE TFs at downregulated DE genes
+    cobindAll = pd.DataFrame() # all DE TFs at all DE genes
+    deGenes = []
+    for i in sortedBeds:
+        factor = i[0].split("/")[-2]
+        try:
+            temp = pd.read_csv(i[count], sep = "\t")
+        except:
+            temp = pd.read_csv(i[0], sep = "\t")
+        temp.sort_values(by = "Ensembl ID", inplace = True)
+        up_genes = temp["Ensembl ID"].isin(up["Ensembl ID"])
+        if factor in up["gene_name"].to_list():
+            cobindUU[factor] = temp.loc[up_genes, "peak_score"]
+            cobindUD[factor] = temp.loc[[not i for i in up_genes], "peak_score"]
+            cobindUp[factor] = temp["peak_score"]
+        else:
+            cobindDU[factor] = temp.loc[up_genes, "peak_score"]
+            cobindDD[factor] = temp.loc[[not i for i in up_genes], "peak_score"]
+            cobindDown[factor] = temp["peak_score"]
+        upDEG = temp.loc[up_genes, "gene_name"].to_list()
+        downDEG = temp.loc[[not i for i in up_genes], "gene_name"].to_list()
+        allDEG = temp["gene_name"].to_list()
+        cobind_Up[factor] = temp.loc[up_genes, "peak_score"]
+        cobind_Down[factor] = temp.loc[[not i for i in up_genes], "peak_score"]
+        cobindAll[factor] = temp["peak_score"]
 
-for i in sortedBeds:
-    factor = i[0].split("/")[-2]
-    temp = pd.read_csv(i[0], sep = "\t")
-    temp.sort_values(by = "Ensembl ID", inplace = True)
-    up_genes = temp["Ensembl ID"].isin(up["Ensembl ID"])
-    if factor in up["gene_name"].to_list():
-        cobindUU[factor] = temp.loc[up_genes, "peak_score"]
-        cobindUD[factor] = temp.loc[~up_genes, "peak_score"]
-        cobindUp[factor] = temp["peak_score"]
-    else:
-        cobindDU[factor] = temp.loc[up_genes, "peak_score"]
-        cobindDD[factor] = temp.loc[~up_genes, "peak_score"]
-        cobindDown[factor] = temp["peak_score"]
-    upDEG = temp.loc[up_genes, "gene_name"].to_list()
-    downDEG = temp.loc[~up_genes, "gene_name"].to_list()
-    allDEG = temp["gene_name"].to_list()
-    cobind_Up[factor] = temp.loc[up_genes, "peak_score"]
-    cobind_Down[factor] = temp.loc[~up_genes, "peak_score"]
-    cobindAll[factor] = temp["peak_score"]
+    cobindUU.index = upDEG
+    cobindDU.index = upDEG
+    cobindUD.index = downDEG
+    cobindDD.index = downDEG
+    cobindUp.index = allDEG
+    cobindDown.index = allDEG
+    cobind_Up.index = upDEG
+    cobind_Down.index = downDEG
+    cobindAll.index = allDEG
 
-cobindUU.index = upDEG
-cobindDU.index = upDEG
-cobindUD.index = downDEG
-cobindDD.index = downDEG
-cobindUp.index = allDEG
-cobindDown.index = allDEG
-cobind_Up.index = upDEG
-cobind_Down.index = downDEG
-cobindAll.index = allDEG
+    os.makedirs(cobindDir, exist_ok = True)
+    cobindUU.to_csv(os.path.join(cobindDir, "UU_" + str(count) + ".csv"), sep = "\t")
+    cobindUD.to_csv(os.path.join(cobindDir, "DD_" + str(count) + ".csv"), sep = "\t")
+    cobindDD.to_csv(os.path.join(cobindDir, "DD_" + str(count) + ".csv"), sep = "\t")
+    cobindDU.to_csv(os.path.join(cobindDir, "DD_" + str(count) + ".csv"), sep = "\t")
+    cobindUp.to_csv(os.path.join(cobindDir, "up_" + str(count) + ".csv"), sep = "\t")
+    cobindDown.to_csv(os.path.join(cobindDir, "down_" + str(count) + ".csv"), sep = "\t")
+    cobind_Up.to_csv(os.path.join(cobindDir, "_up_" + str(count) + ".csv"), sep = "\t")
+    cobind_Down.to_csv(os.path.join(cobindDir, "_down_" + str(count) + ".csv"), sep = "\t")
+    cobindAll.to_csv(os.path.join(cobindDir, "all_" + str(count) + ".csv"), sep = "\t")
 
-os.makedirs(cobindDir, exist_ok = True)
-cobindUU.to_csv(os.path.join(cobindDir, "UU_train.csv"), sep = "\t")
-cobindUD.to_csv(os.path.join(cobindDir, "DD_train.csv"), sep = "\t")
-cobindDD.to_csv(os.path.join(cobindDir, "DD_train.csv"), sep = "\t")
-cobindDU.to_csv(os.path.join(cobindDir, "DD_train.csv"), sep = "\t")
-cobindUp.to_csv(os.path.join(cobindDir, "up_train.csv"), sep = "\t")
-cobindDown.to_csv(os.path.join(cobindDir, "down_train.csv"), sep = "\t")
-cobind_Up.to_csv(os.path.join(cobindDir, "_up_train.csv"), sep = "\t")
-cobind_Down.to_csv(os.path.join(cobindDir, "_down_train.csv"), sep = "\t")
-cobindAll.to_csv(os.path.join(cobindDir, "all_train.csv"), sep = "\t")
-
-
-# compile cobinding maps for different conditions
-cobindUU = pd.DataFrame() # upregulated TFs at upregulated DE genes
-cobindUD = pd.DataFrame() # upregulated TFs at downregulated DE genes
-cobindDD = pd.DataFrame() # downregulated TFs at downregulated DE genes
-cobindDU = pd.DataFrame() # downregulated TFs at upregulated DE genes
-cobindUp = pd.DataFrame() # upregulated TFs at all DE genes
-cobindDown = pd.DataFrame() # downregulated TFs at all DE genes
-cobind_Up = pd.DataFrame() # all DE TFs at upregulated DE genes
-cobind_Down = pd.DataFrame() # all DE TFs at downregulated DE genes
-cobindAll = pd.DataFrame() # all DE TFs at all DE genes
-deGenes = []
-
-for i in sortedBeds:
-    factor = i[0].split("/")[-2]
-    temp = pd.read_csv(i[0], sep = "\t")
-    # use second highest ranked file if available to create "test" set
-    if len(i) > 1:
-        temp = pd.read_csv(i[1], sep = "\t")
-    temp.sort_values(by = "Ensembl ID", inplace = True)
-    up_genes = temp["Ensembl ID"].isin(up["Ensembl ID"])
-    if factor in up["gene_name"].to_list():
-        cobindUU[factor] = temp.loc[up_genes, "peak_score"]
-        cobindUD[factor] = temp.loc[[not i for i in up_genes], "peak_score"]
-        cobindUp[factor] = temp["peak_score"]
-    else:
-        cobindDU[factor] = temp.loc[up_genes, "peak_score"]
-        cobindDD[factor] = temp.loc[[not i for i in up_genes], "peak_score"]
-        cobindDown[factor] = temp["peak_score"]
-    upDEG = temp.loc[up_genes, "gene_name"].to_list()
-    downDEG = temp.loc[[not i for i in up_genes], "gene_name"].to_list()
-    allDEG = temp["gene_name"].to_list()
-    cobind_Up[factor] = temp.loc[up_genes, "peak_score"]
-    cobind_Down[factor] = temp.loc[[not i for i in up_genes], "peak_score"]
-    cobindAll[factor] = temp["peak_score"]
-
-cobindUU.index = upDEG
-cobindDU.index = upDEG
-cobindUD.index = downDEG
-cobindDD.index = downDEG
-cobindUp.index = allDEG
-cobindDown.index = allDEG
-cobind_Up.index = upDEG
-cobind_Down.index = downDEG
-cobindAll.index = allDEG
-
-os.makedirs(cobindDir, exist_ok = True)
-cobindUU.to_csv(os.path.join(cobindDir, "UU_test.csv"), sep = "\t")
-cobindUD.to_csv(os.path.join(cobindDir, "DD_test.csv"), sep = "\t")
-cobindDD.to_csv(os.path.join(cobindDir, "DD_test.csv"), sep = "\t")
-cobindDU.to_csv(os.path.join(cobindDir, "DD_test.csv"), sep = "\t")
-cobindUp.to_csv(os.path.join(cobindDir, "up_test.csv"), sep = "\t")
-cobindDown.to_csv(os.path.join(cobindDir, "down_test.csv"), sep = "\t")
-cobind_Up.to_csv(os.path.join(cobindDir, "_up_test.csv"), sep = "\t")
-cobind_Down.to_csv(os.path.join(cobindDir, "_down_test.csv"), sep = "\t")
-cobindAll.to_csv(os.path.join(cobindDir, "all_test.csv"), sep = "\t")
-
-# output a list of DE gene promoters that had no annotated TF binding
-noBind = cobindAll[(cobindAll==0).all(axis = 1)]
-with open(os.path.join(cobindDir, "noBinding.csv"), "w") as out:
-    out.writelines([i + '\n' for i in noBind.index])
+    # output a list of DE gene promoters that had no annotated TF binding
+    noBind = cobindAll[(cobindAll==0).all(axis = 1)]
+    with open(os.path.join(cobindDir, "noBinding.csv"), "w") as out:
+        out.writelines([i + '\n' for i in noBind.index])
